b6dd67cb0d2c23c49879f35676275d5663b6dfd1
DaniGCaff <danigcaff@gmail.com>
2018-01-21 10:24:32 +0100

    Ficheros pendientes

---
+++ CircuitPlanification.RepositoryModels/AirportCoords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitPlanification.RepositoryModels
+{
+    public class AirportCoords
+    {
+        public string latitud { get; set; }
+        public string longitud { get; set; }
+    }
+}
